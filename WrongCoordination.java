a6dbed61c66e7448b441ef97b712e55cfef4f884
ashwaqmatar <33615853+ashwaqmatar@users.noreply.github.com>
2017-12-21 22:46:13 +0200

    Create WrongCoordination.java

---
+++ WrongCoordination.java
@@ -0,0 +1,20 @@
+package testPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ConverterPackage.Router;
+import ConverterPackage.router_Analyst;
+
+
+public class WrongCoordination {
+	
+	@Test
+	public void test() {
+		Router router = new Router("c4:6e:1f:30:2b:7c","routerName","10-27-2017 4:15:52 PM",0,91,0,0);
+		router_Analyst analyst = new router_Analyst();
+		assertEquals(false,analyst.analyze(router));	
+		}
+
+}

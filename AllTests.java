bda0a0e19b2cd627a300b3c3ff1f66090499ed05
ashwaqmatar <33615853+ashwaqmatar@users.noreply.github.com>
2017-12-21 22:43:41 +0200

    Update AllTests.java

--- AllTests.java
+++ AllTests.java
@@ -1,14 +1,11 @@
 package testPackage;
 
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestCSVFilter.class, TestKMLFilter.class })
+@SuiteClasses({ TestRightRouter.class, WrongMAC.class ,WrongMAC2.class ,WrongCoordination.class ,WrongCoordination2.class})
 public class AllTests {
 
 }
-
-

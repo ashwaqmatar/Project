b23384cfb4e410951b2eeb172f8046a2015d8d88
ashwaqmatar <33615853+ashwaqmatar@users.noreply.github.com>
2017-12-21 22:27:57 +0200

    Update CSVBuilder.java

--- CSVBuilder.java
+++ CSVBuilder.java
@@ -1,19 +1,35 @@
-package add;
+package ConverterPackage;
+
 public class CSVBuilder implements FileBuilder {
 
 	@Override
-	public String Exit( ){
+	public String Create() {
+		String ans = "Latitude,Longitude,AltitudeMeters,#wifinetworks";
+		for(int indx=1;indx<=10;indx++) {
+			ans +=",SSID"+indx+",MAC"+indx+",Signal"+indx+",FirstSeen"+indx+",Timestamp"+indx;
+		}
+		ans+="\n";
+		return ans;
+	}
+
+	@Override
+	public String Finish() {
+		// TODO Auto-generated method stub
 		return "";
 	}
-	
+
 	@Override
-	public String Create( ){
-		return ("SSID,MAC,AuthMode,Frequency,Timestamp,Date,CurrentLongitude,CurrentLatitude");
-	}
-	
-	@Override
-	public String addInfo(Object allInfo){
-		return ((String) allInfo);
+	public String addInfo(Object Data) {
+		String ans="";
+		if(Data instanceof Router) {
+			Router router = (Router) Data;
+			ans+=router.getSSID()+","+router.getMAC()+","+router.getRSSI()+","+router.getFirstSeen()+","+router.getTimestamp()+",";
+		}
+		else {
+			if(Data instanceof String)
+				ans = ans+Data+",#wifinetworks,";
+		}
+		return ans;
 	}
 
 }
